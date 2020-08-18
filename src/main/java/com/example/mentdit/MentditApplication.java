76e03599f5add04991aab16b3b8fe1570cd8beb9
p-papag <thedrpapa@gmail.com>
2020-08-18 12:55:05 +0300

    Initial build commit

---
+++ src/main/java/com/example/mentdit/MentditApplication.java
@@ -0,0 +1,13 @@
+package com.example.mentdit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mentdit/MentditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mentdit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
